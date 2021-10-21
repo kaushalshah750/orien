0ae9f722bfb102bf0210e488a471ac102e13e739
kaushal shah <kaushalshah750@gmail.com>
2021-10-21 23:19:57 +0530

    Orien

---
+++ js/script.js
@@ -0,0 +1,59 @@
+// Detect request animation frame
+var scroll = window.requestAnimationFrame ||
+// IE Fallback
+function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+
+function loop() {
+
+Array.prototype.forEach.call(elementsToShow, function(element){
+ if (isElementInViewport(element)) {
+   element.classList.add('is-visible');
+ } else {
+   element.classList.remove('is-visible');
+ }
+});
+
+scroll(loop);
+}
+
+// Call the loop for the first time
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+// special bonus for those using jQuery
+if (typeof jQuery === "function" && el instanceof jQuery) {
+el = el[0];
+}
+var rect = el.getBoundingClientRect();
+return (
+(rect.top <= 0
+ && rect.bottom >= 0)
+||
+(rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+ rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+||
+(rect.top >= 0 &&
+ rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+);
+}
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
